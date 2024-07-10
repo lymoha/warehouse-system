5afb20166be3d3f751727eabcb7242fbbb27be92
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-07-10 10:31:34 +0200

    add Header, Homepage, Routing

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return (
+        <>
+            <h1 className="app-header">Room Ranger 3000</h1>
+            <h2 className="app-header-2">Clean up your life</h2>
+        </>
+    );
+}
