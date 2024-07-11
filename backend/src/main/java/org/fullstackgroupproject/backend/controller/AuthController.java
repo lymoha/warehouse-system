b6ae016c509e60cf082f3a551a354dae5195d5f7
ursulamontag <montagu666@gmail.com>
2024-07-11 12:05:59 +0200

    Add backend security with Oauth

---
+++ backend/src/main/java/org/fullstackgroupproject/backend/controller/AuthController.java
@@ -0,0 +1,17 @@
+package org.fullstackgroupproject.backend.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @GetMapping("/me")
+    public String getMe(@AuthenticationPrincipal OAuth2User user) {
+        return user.getName();
+    }
+}
