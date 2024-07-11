3f32d8ef8d9c8fc36fcf0a2e137ae4ccaf1643b4
ChrizDope <incubusid@googlemail.com>
2024-07-11 16:12:46 +0200

    add frontend get-route + css + ItemCard + ItemGalleryPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,25 @@ import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import AddPage from "./pages/AddPage.tsx";
-import {DtoItem} from "./types/Item.ts";
+import {DtoItem, Item} from "./types/Item.ts";
 import axios from "axios";
+import {useEffect, useState} from "react";
+import ItemGalleryPage from "./pages/ItemGalleryPage.tsx";
 
 
 export default function App() {
 
+    const [items, setItems] = useState<Item[]>([])
+
     const addItem = (newItem: DtoItem) => {
         axios.post("/api/add", newItem)
             .catch(error => console.error("error adding item: ", error));
+    };
+
+    const getAllItems = () => {
+        axios.get("/api")
+            .then(response => {setItems(response.data)})
+            .catch(error => console.error("something went wrong", error))
     };
 
     const router = createBrowserRouter([
@@ -22,7 +32,15 @@ export default function App() {
             path: "/add",
             element: <AddPage addItem={addItem}/>
         },
+        {
+            path: "/gallery",
+            element: <ItemGalleryPage items={items}/>
+        },
     ])
+
+    useEffect(() => {
+        getAllItems();
+    }, []);
 
     return (
         <>
---
+++ frontend/src/pages/ItemGalleryPage.tsx
@@ -0,0 +1,22 @@
+import {Item} from "../types/Item.ts";
+import ItemCard from "../components/ItemCard.tsx";
+import Header from "../components/Header.tsx";
+import "../styles/ItemGalleryPage.css"
+
+type ItemGalleryPageProps = {
+    items:Item[],
+}
+
+export default function ItemGalleryPage(props:Readonly<ItemGalleryPageProps>) {
+    return (
+        <>
+            <Header/>
+            <div className="gallery">
+                {props.items.map(item => (
+                    <ItemCard key={item.id} item={item}/>
+
+                ))}
+            </div>
+        </>
+    );
+}
