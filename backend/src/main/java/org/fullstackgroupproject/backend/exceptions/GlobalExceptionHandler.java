1e8b7e2794c4600b7e8f8fa828249219114feb84
julianeschrittenlacher <julianeschrittenlacher@gmail.com>
2024-07-12 16:23:30 +0200

    add controller, service get and update by id and exception handling

--- backend/src/main/java/org/fullstackgroupproject/backend/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/org/fullstackgroupproject/backend/exceptions/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +22,18 @@ public class GlobalExceptionHandler {
                 exception.getMessage(),
                 LocalDateTime.now()
         );
+    }
+
+    @ExceptionHandler(InvalidIdException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponseDTO handleNoSuchElementException(InvalidIdException exception, WebRequest webRequest) {
+        return new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+
     }
 
 //    @ExceptionHandler(Exception.class)
