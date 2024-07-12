a00e79016ae9987e089d0b82960cd452deeb43da
ChrizDope <incubusid@googlemail.com>
2024-07-12 14:54:54 +0200

    fix GlobalExceptionHandlerTest

--- backend/src/test/java/org/fullstackgroupproject/backend/exceptions/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/org/fullstackgroupproject/backend/exceptions/GlobalExceptionHandlerTest.java
@@ -1,14 +1,13 @@
 package org.fullstackgroupproject.backend.exceptions;
 
-import org.fullstackgroupproject.backend.controller.RoomRangerController;
 import org.fullstackgroupproject.backend.model.DtoItem;
 import org.fullstackgroupproject.backend.service.RoomService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,8 +15,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = RoomRangerController.class)
-@Import(GlobalExceptionHandler.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class GlobalExceptionHandlerTest {
 
     @Autowired
@@ -29,29 +28,17 @@ public class GlobalExceptionHandlerTest {
     @Test
     public void testHandleNullPointerException() throws Exception {
         // Wenn der Service mit der Fake-Datenbank (Mockito) aufgerufen wird und ein Item vom Typ DtoItem hinzugefügt
-        // wird eine NullPointerException geworfen
+        // wird eine Exception geworfen
         Mockito.when(roomService.addItem(Mockito.any(DtoItem.class))).thenThrow(new NullPointerException("This is a NullPointerException"));
 
         // Fake-Post auf route /api/add
         mockMvc.perform(MockMvcRequestBuilders.post("/api/add")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"name\":\"testItem\"}"))
-                // Nullpointer schickt den Status badRequest
-                .andExpect(status().isBadRequest())
+                .andExpect(status().isInternalServerError())
                 .andExpect(jsonPath("$.errorMsg").value("This is a NullPointerException"))
-                .andExpect(jsonPath("$.errorCode").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.errorCode").value("INTERNAL_SERVER_ERROR"))
                 .andExpect(jsonPath("$.timestamp").exists())
                 .andExpect(jsonPath("$.apiPath").exists());
-    }
-
-    @Test
-    public void testHandleGeneralException() throws Exception {
-        Mockito.when(roomService.addItem(Mockito.any(DtoItem.class))).thenThrow(new RuntimeException("This is a general exception"));
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"name\":\"testItem\"}"))
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$").value("This is a general exception"));
     }
 }
