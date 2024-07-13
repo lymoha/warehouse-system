931f93229f5d377bcf91a6bdb8cd58f99c64b33f
Mohammed Ly <lybhoye@yahoo.com>
2024-07-13 11:54:10 +0200

    change appstyling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,12 @@
+import Navigation from "./Navigation.tsx";
+
 export default function Header() {
 
     return (
         <>
             <h1 className="app-header">Room Ranger 3000</h1>
             <h2 className="app-header-2">Clean up your life</h2>
+            <Navigation/>
         </>
     );
 }
